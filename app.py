6560c1313db526db7e1ccd56f243d246b1a79e67
Flockyy <florian.abgrall750@gmail.com>
2022-01-13 16:11:25 +0100

    'uesh'

---
+++ app.py
@@ -0,0 +1,6 @@
+from website import create_app
+
+app = create_app()
+
+if __name__ == '__name_':
+    app.run(debug = True)
--- base_test.sql
+++ base_test.sql
@@ -1,32 +1,31 @@
 DROP DATABASE IF EXISTS Candidature_simplon;
 
 CREATE DATABASE Candidature_simplon;
-USE Candidature_simplon;
 
 CREATE TABLE User (
-    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     last_name VARCHAR(50) NOT NULL,
     first_name VARCHAR(50) NOT NULL,
     telephone_number VARCHAR(50),
     e_mail VARCHAR(50) NOT NULL,
     password VARCHAR(50) NOT NULL,
-    is_admin BOOLEAN NOT NULL
+    is_admin BOOL NOT NULL
 );
 
 CREATE TABLE Entreprise (
-    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(100) NOT NULL,
     place VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE Candidature (
-    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    user_id INT NOT NULL,
-    enterprise_id INT NULL,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    enterprise_id INTEGER NULL,
     contact VARCHAR(100)
 );
 
-INSERT INTO User (last_name,first_name,telephone_number,e_mail,password,is_admin) VALUES 
+INSERT INTO User (last_name, first_name, telephone_number, e_mail, password, is_admin) VALUES 
 ("Bourez", "Rudy", "06.06.06.06.06","mail@mail.com", "******", false),
 ("Hamour", "Yanis", "06.06.06.06.06","mail@mail.com", "******", false),
 ("Durand", "Brune", "","mail@mail.com", "******", true),
@@ -36,14 +35,13 @@ INSERT INTO User (last_name,first_name,telephone_number,e_mail,password,is_admin
 ("Druesne", "Steven", "06.06.06.06.06","mail@mail.com", "******", false),
 ("Haddou", "Ayoub", "06.06.06.06.06","mail@mail.com", "******", false);
 
-INSERT INTO Entreprise (name,place) VALUES
+INSERT INTO Entreprise (name, place) VALUES
 ("Urluberlu", "Lille"),
 ("Taratata", "Lens"),
 ("Turlututu", "Seclin"),
 ("Rondoudou", "Marcq-en-Bareuil");
 
-
-INSERT INTO Candidature (user_id,enterprise_id,contact) VALUES
+INSERT INTO Candidature (user_id, enterprise_id, contact) VALUES
 (0,0,"John Doe"),
 (1,1,"Robert"),
 (5,2,""),
