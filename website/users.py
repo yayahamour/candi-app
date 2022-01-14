354c8125fae0e5bc0d8948f43ce664e46b7e3423
Steven <stevendruesne@gmail.com>
2022-01-14 11:04:11 +0100

    Added add nomination form and query

--- website/users.py
+++ website/users.py
@@ -1,6 +1,8 @@
-from dataclasses import dataclass 
+from dataclasses import dataclass
+import imp 
 import sqlite3
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request, flash, redirect, url_for
+from form_add import LoginForm
 
 app = Flask(__name__)
 
@@ -47,7 +49,36 @@ class Apprenant(Users):
     is_admin = False
 
     def add_nomination():
-        pass 
+        pass
+        form_add = LoginForm()
+        if form_add.validate_on_submit():
+            entreprise = request.form["Entreprise"]
+            lieu = request.form["Lieu"]
+            contact = request.form["Contact"]
+            # date = request.form["Date"]
+            # date_de_relance = request.form["Date de relance"]
+            # statut = request.form["Statut"]
+
+            c = sqlite3.connect('website/DB/base_test.db')
+            nomination = c.cursor()
+            # check if entreprise is in Entreprise table
+            is_entreprise = nomination.execute("SELECT id, name FROM Entreprise WHERE name = ?", (entreprise,))
+            if not is_entreprise :
+                # add new enterprise in Entreprise table
+                nomination.execute("INSERT INTO Entreprise (name, lieu) VALUES (?,?)", (entreprise, lieu,))
+                new_enterprise_id = ("SELECT MAX(id) FROM Entreprise")
+                # add new nomination in Candidature table
+                nomination.execute("INSERT INTO Candidature (user_id, enterprise_id, contact) VALUES (?, ?, ?)", (user_id, new_enterprise_id, contact,))
+            else :
+                enterprise_id = is_entreprise[0]
+                nomination.execute("INSERT INTO Candidature (user_id, enterprise_id, contact)",(user_id, enterprise_id, contact,))
+            c.commit()
+
+            flash('Candidature ajoutée pour {}'.format(form_add.enterprise.data))
+            
+            return redirect(url_for('board'))
+        return render_template('board.html', form_add=form_add)
+     
 
     def modify_nomination(self):
         enterprise = 1 # Enterprise_id obtained from HTML attribute
