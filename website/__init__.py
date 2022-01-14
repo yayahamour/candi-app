fc7a0c79d108ac75d7bee38584ad459e357b1913
RudyBourez <bourez.rudy@gmail.com>
2022-01-14 16:32:45 +0100

    er

--- website/__init__.py
+++ website/__init__.py
@@ -7,8 +7,10 @@ def create_app():
     
     from .views import views
     from .auth import auth
+    from .users import users
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+    app.register_blueprint(users, url_prefix='/')
 
     return app
