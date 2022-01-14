288ac26e596d9184da8952d3258123c1fe330ab0
RudyBourez <bourez.rudy@gmail.com>
2022-01-14 09:29:47 +0100

    mise en commun

--- website/auth.py
+++ website/auth.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 # from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, login_required, logout_user, current_user
+from website.users import Apprenant, Admin
 import sqlite3
 
 connection = sqlite3.connect('website/DB/base_test.db', check_same_thread=False)
@@ -9,16 +10,6 @@ cur = connection.cursor()
 
 auth = Blueprint('auth', __name__)
 
-@dataclass
-class User:
-    id: int
-    last_name: str
-    first_name: int
-    telephone: str
-    e_mail: str
-    password: int
-    is_admin: bool
-    is_active: bool
     
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -29,10 +20,12 @@ def login():
         cur.execute("SELECT * FROM User WHERE e_mail = ?",
                     (email,)
                     )
-        
         row = cur.fetchone()
         if row:
-            user = User(row[0], row[1], row[2], row[3], row[4], row[5], row[6], is_active=False)
+            if row[7] == False:
+                user = Apprenant(row[0],row[1],row[2],row[4],row[5],row[3])
+            else:
+                user = Admin(row[0],row[1],row[2],row[4],row[5])
             if user:
                 if user.password == password:
                     flash('Logged in successfully!', category='success')
