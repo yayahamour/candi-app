6bf81e3d58b6cd4fb328e7b62d5373965494acb0
Yanis <yayahamour@gmail.com>
2022-06-24 16:26:14 +0200

    er

--- app.py
+++ app.py
@@ -2,13 +2,17 @@ from App import app
 import sentry_sdk
 import logging
 from opencensus.ext.azure.log_exporter import AzureLogHandler
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
 if __name__ == '__main__':
     logger = logging.getLogger(__name__)
     logger.addHandler(AzureLogHandler(
     connection_string='InstrumentationKey=00000000-0000-0000-0000-000000000000')
 )
-    logger.warning("Sucescs")
+    logger.warning("Sucesss")
+    assert 1 == 0
     sentry_sdk.init(
         dsn="https://432bbda901094e48b34d4c6a51c6a28b@o1298037.ingest.sentry.io/6527720",
 
@@ -17,6 +21,7 @@ if __name__ == '__main__':
         # We recommend adjusting this value in production.
         traces_sample_rate=1.0
     )
+    
 
     app.run(host='0.0.0.0', port=8000, debug=True)
 
---
+++ test_pytest.py
@@ -0,0 +1,3 @@
+def test_azure():
+    assert 8 == 8
+    assert 8 == 7
