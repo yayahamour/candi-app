a424baebaae2856a9baa5de686b3d9e87ce1e404
Yanis <yayahamour@gmail.com>
2022-07-01 11:37:59 +0200

    azure

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from App import app
-import sentry_sdk
 import logging
 from opencensus.ext.azure.log_exporter import AzureLogHandler
 from selenium import webdriver
@@ -12,15 +11,7 @@ if __name__ == '__main__':
     connection_string='InstrumentationKey=00000000-0000-0000-0000-000000000000')
 )
     logger.warning("Sucesss")
-    sentry_sdk.init(
-        dsn="https://432bbda901094e48b34d4c6a51c6a28b@o1298037.ingest.sentry.io/6527720",
-
-        # Set traces_sample_rate to 1.0 to capture 100%
-        # of transactions for performance monitoring.
-        # We recommend adjusting this value in production.
-        traces_sample_rate=1.0
-    )
-    
+   
 
     app.run(host='0.0.0.0', port=8000, debug=True)
 
--- test_pytest.py
+++
@@ -1,3 +0,0 @@
-def test_azure():
-    assert 8 == 8
-    assert 8 == 7
